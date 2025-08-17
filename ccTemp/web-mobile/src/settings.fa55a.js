25f2025ae71663b9f92657693848f302db43e721
yby <st@gmail.com>
2025-08-18 00:18:07 +0800

    mobile-web

--- ccTemp/web-mobile/src/settings.fa55a.js
+++
@@ -1,1 +0,0 @@
-window._CCSettings={platform:"web-mobile",groupList:["default"],collisionMatrix:[[true]],hasResourcesBundle:true,hasStartSceneBundle:false,remoteBundles:[],subpackages:[],launchScene:"db://assets/Scene/Game.fire",orientation:"",jsList:[],bundleVers:{internal:"d17e2",resources:"6d95e",Common:"9f1ed",LoadingRes:"0e63d",main:"05353"}};
