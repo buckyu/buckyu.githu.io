923890843a978339cf3bef6a2b90e55cc648c802
yby <st@gmail.com>
2025-08-23 10:13:05 +0800

    up

---
+++ ccTemp/web-mobile/src/settings.f5875.js
@@ -0,0 +1,1 @@
+window._CCSettings={platform:"web-mobile",groupList:["default"],collisionMatrix:[[true]],hasResourcesBundle:true,hasStartSceneBundle:false,remoteBundles:[],subpackages:[],launchScene:"db://assets/Scene/Game.fire",orientation:"portrait",jsList:[],bundleVers:{internal:"d17e2",resources:"6d95e",Common:"2a553",LoadingRes:"fc800",main:"8895b"}};
